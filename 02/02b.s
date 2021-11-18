3d1247a40323f053bd32b7337e9abf6f5d450255
Milo Fultz <milopfultz@gmail.com>
2021-11-18 19:23:00 +0700

    Solve for Part 2

--- 02/02b.s
+++ 02/02b.s
@@ -7,7 +7,7 @@ enum $00                        ; Declare memory for variables
   width rBYTE 1
   height rBYTE 1
   isSorted rBYTE 1
-  factor rBYTE 1
+  product rBYTE 1
   ribbon rBYTE 1
   temp rBYTE 1
 ende
@@ -33,85 +33,94 @@ ende
 ; $0a    = LF (\n)
 ; $78    = 'x'
 
-;Part2:
-;  lda #0                        ; Init accumulator to 0
-;  tax                           ; Init X register to 0 (pointer to L,W,H)
-;  tay                           ; Init Y register to 0 (memory pointer)
-;
-;  GetNextChar:
-;  lda ($10),y                   ; Load dereferenced value $0010 into accumulator
-;  inc memory                    ; Increment memory LSD
-;  bne ContinueGetNextChar       ; If memory LSD did not exceed $ff, continue
-;  jsr IncrementHighMemory       ; Else, increment memory MSD
-;
-;  ContinueGetNextChar:
-;  cmp #0                        ; If value is zero,
-;  beq Infinite                  ;   Goto Infinite (end of input)
-;  cmp #$78                      ; If accumulator represents an 'x',
-;  beq StoreNum                  ;   Goto StoreNum
-;  sec
-;  sbc #$30                      ; If accumulator represents a number,
-;  bcs CompileNum                ;   Goto CompileNum (acc contains number)
-;                                ; Else if accumulator represents a newline,
-;  AddAreaLSD:
-;  jsr FindArea                  ; Calculate the total area in `area`
-;  lda total + 2                 ; Load total LSD into accumulator
-;  clc
-;  adc area + 1                  ; Add area LSD to total LSD
-;  sta total + 2                 ; Store result in total LSD
-;  bcc AddAreaSD                 ; If sum didn't exceed $ff, continue
-;  jsr IncrementTotalSD          ; Else, increment total SD
-;
-;  AddAreaSD:
-;  lda total + 1                 ; Load total SD into accumulator
-;  clc
-;  adc area                      ; Add area MSD to total SD
-;  sta total + 1                 ; Store result in total SD
-;  bcc ContinueAddAreaSD         ; If sum didn't exceed $ff, continue
-;  jsr IncrementTotalMSD         ; Else, increment total MSD
-;
-;  ContinueAddAreaSD:
-;  lda #0                        ; Re-initialize L,W,H and X/Y register
-;  sta length
-;  sta width
-;  sta height
-;  tax
-;  tay
-;  jmp GetNextChar               ; Start next box
-;
-;  StoreNum:
-;  inx                           ; Point to next side (L,W,H)
-;  jmp GetNextChar               ; Get next character
-;
-;  CompileNum:
-;  pha                           ; Save current number
-;  txa
-;  pha                           ; Save current pointer to L,W,H
-;  tay
-;  ldx $05,y                     ; Load tens-place of number into X register
-;  ldy #10                       ; Load 10 into Y register
-;  jsr Multiply                  ; Multiply tens-place number by 10
-;  pla                           ; Retrieve current pointer to L,W,H
-;  tax                           ; Store it in X register
-;  pla                           ; Retrieve current number
-;  clc
-;  adc product + 1               ; Add current number to product LSD
-;  sta $05,x                     ; Store sum at current side (L,W,H)
-;  jmp GetNextChar               ; Get next character
-;
-;  IncrementHighMemory:
-;  inc memory + 1                ; Increment memory MSD
-;  rts                           ; Return from subroutine
-;
-;  IncrementTotalSD:
-;  inc total + 1                 ; Increment total SD
-;  beq IncrementTotalMSD         ; If total + 1 crossed from $ff -> $00,
-;                                ;   Goto IncrementTotalMSD
-;  rts
-;
-;  IncrementTotalMSD:
-;  inc total                     ; Increment total MSD
-;  rts
+Part2:
+  lda #0                        ; Init accumulator to 0
+  tax                           ; Init X register to 0 (pointer to L,W,H)
+  tay                           ; Init Y register to 0 (memory pointer)
+
+  GetNextChar:
+  lda ($10),y                   ; Load dereferenced value $0010 into accumulator
+  inc memory                    ; Increment memory LSD
+  bne ContinueGetNextChar       ; If memory LSD did not exceed $ff, continue
+  jsr IncrementHighMemory       ; Else, increment memory MSD
+
+  ContinueGetNextChar:
+  cmp #0                        ; If value is zero,
+  beq Infinite                  ;   Goto Infinite (end of input)
+  cmp #$78                      ; If accumulator represents an 'x',
+  beq StoreNum                  ;   Goto StoreNum
+  sec
+  sbc #$30                      ; If accumulator represents a number,
+  bcs CompileNum                ;   Goto CompileNum (acc contains number)
+                                ; Else if accumulator represents a newline,
+  jsr GetVolume                 ; Calculate the total volume of L,W,H
+  jsr GetRibbon                 ; Calculate the total ribbon
+
+  AddRibbon:
+  lda total + 2                 ; Load total LSD into accumulator
+  clc
+  adc ribbon
+  sta total + 2                 ; Store result in total LSD
+  bcc AddVolumeLSD              ; If sum didn't exceed $ff, continue
+  jsr IncrementTotalSD          ; Else, increment total SD
+
+  AddVolumeLSD:
+  clc
+  adc volume + 1                ; Add volume LSD to total LSD
+  sta total + 2                 ; Store result in total LSD
+  bcc AddVolumeSD               ; If sum didn't exceed $ff, continue
+  jsr IncrementTotalSD          ; Else, increment total SD
+
+  AddVolumeSD:
+  lda total + 1                 ; Load total SD into accumulator
+  clc
+  adc volume                    ; Add volume MSD to total SD
+  sta total + 1                 ; Store result in total SD
+  bcc ContinueAddVolumeSD       ; If sum didn't exceed $ff, continue
+  jsr IncrementTotalMSD         ; Else, increment total MSD
+
+  ContinueAddVolumeSD:
+  lda #0                        ; Re-initialize L,W,H and X/Y register
+  sta length
+  sta width
+  sta height
+  tax
+  tay
+  jmp GetNextChar               ; Start next box
+
+  StoreNum:
+  inx                           ; Point to next side (L,W,H)
+  jmp GetNextChar               ; Get next character
+
+  CompileNum:
+  pha                           ; Save current number
+  txa
+  pha                           ; Save current pointer to L,W,H
+  tay
+  ldx $05,y                     ; Load tens-place of number into X register
+  ldy #10                       ; Load 10 into Y register
+  jsr Multiply                  ; Multiply tens-place number by 10
+  pla                           ; Retrieve current pointer to L,W,H
+  tax                           ; Store it in X register
+  pla                           ; Retrieve current number
+  clc
+  adc product + 1               ; Add current number to product LSD
+  sta $05,x                     ; Store sum at current side (L,W,H)
+  jmp GetNextChar               ; Get next character
+
+  IncrementHighMemory:
+  inc memory + 1                ; Increment memory MSD
+  rts                           ; Return from subroutine
+
+  IncrementTotalSD:
+  inc total + 1                 ; Increment total SD
+  beq IncrementTotalMSD         ; If total + 1 crossed from $ff -> $00,
+                                ;   Goto IncrementTotalMSD
+  rts
+
+  IncrementTotalMSD:
+  inc total                     ; Increment total MSD
+  rts
 
 
 
@@ -138,16 +147,16 @@ ende
 ;  jsr GetVolume                 ; Result should be #$1770/#6000
 ;  jmp Infinite
 ;
-  ; GetRibbon
-  lda #20
-  sta length
-  lda #30
-  sta width
-  lda #10
-  sta height
-
-  jsr GetRibbon                ; Result should be #$3C/#60
-  jmp Infinite
+;  ; GetRibbon
+;  lda #20
+;  sta length
+;  lda #30
+;  sta width
+;  lda #10
+;  sta height
+;
+;  jsr GetRibbon                ; Result should be #$3C/#60
+;  jmp Infinite
 
 
 Infinite:
@@ -157,6 +166,26 @@ Infinite:
 ;
 ; Helper Subroutines
 ;
+
+Multiply:
+  ; IN:  X (number), Y (iterator) as two numbers to multiply
+  ; OUT: none (updated `product`)
+  lda #0
+  sta product + 1               ; Initialize product to 0
+  sta product
+
+  AddNum:
+  txa                           ; Put number in accumulator
+  clc
+  adc product + 1               ; Sum product and number in accumulator
+  bcc StoreNewProduct           ; If sum didnt exceed #$ff, goto StoreNewProduct
+  inc product                   ; Else, increment second place of product
+
+  StoreNewProduct:
+  sta product + 1               ; Store new sum in product
+  dey                           ; Decrement iterator
+  bne AddNum                    ; If iterator is not zero, goto AddNum
+  rts                           ; Else, return from subroutine
 
 GetTwoSmallest:
   ; IN:  length, width, and height values
@@ -227,7 +256,6 @@ GetVolume:
 
   PrepMultiplyMSD:
   lda #0                        ; Initialize accumulator to 0
-  beq PrepMultiplyLSD           ; If volume MSD is 0, skip it
   ldy $00,x                     ; Else, get current dimension value
   jsr MultiplyVolumeMSD         ; Multiply current value by the volume MSD
 
